2f91e46b1407abab4084fb856ecba9e07bddb8cc
bg14ff <55153211+bg14ff@users.noreply.github.com>
2019-11-08 10:13:14 -0500

    Lab8

---
+++ Lab8/Lab8Activity/Lab8Activity.js
@@ -0,0 +1,11 @@
+function styleswap(){
+
+document.getElementById('one').style.color="blue";
+document.getElementById('one').style.fontSize="30px";
+document.getElementById('two').style.color="green";
+document.getElementById('two').style.fontSize="20px";
+document.getElementById('two').style.marginLeft="150px";
+document.getElementById('three').style.textTransform="uppercase";
+document.getElementById('three').style.color="orange";
+
+}
---
+++ Lab8/Lab8E2.js
@@ -0,0 +1,17 @@
+//Sets the initial colours of the three elements on page load
+document.getElementById('blue').style.color="blue";
+document.getElementById('green').style.color="green";
+document.getElementById('red').style.color="red";
+
+//This will output when a button is pressed
+function mix(){
+
+	var first = "#00BB00";
+	var second = "BB9320";
+	var third = "#AB00FF";
+
+	document.getElementById('blue').style.color=first;
+	document.getElementById('green').style.colour=second;
+	document.getElementById('red').style.color=third;
+
+}
