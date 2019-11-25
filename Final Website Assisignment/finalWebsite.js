3e1b1cc0cf1915263f4b5ad00cf850288ae7ad6e
bg14ff <55153211+bg14ff@users.noreply.github.com>
2019-11-25 14:31:34 -0500

    Last changes before changing direct to myportfolio

--- Final Website Assisignment/finalWebsite.js
+++ Final Website Assisignment/finalWebsite.js
@@ -4,5 +4,9 @@ function theRest()
 
 document.getElementById("output2").innerHTML="Because he wanted to be a hot dog!";
 }
+//This will output when a button is pressed
+function theRest2()
+{
 
-//The write zero line was removed to remove error
+document.getElementById("output2").innerHTML="Because she's always running away from the ball!";
+}
